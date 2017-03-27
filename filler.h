313222d1efaf0f439ac3e4bb4722942f37867327
Thibaut FONTAINE <tfontain@e2r11p10.42.fr>
2017-03-27 11:24:02 +0200

    all

---
+++ filler.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/27 11:20:10 by tfontain          #+#    #+#             */
+/*   Updated: 2017/03/27 11:23:43 by tfontain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLER_H
+# define FILLER_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+
+
+
+#endif
