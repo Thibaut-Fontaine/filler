1c2293b5d7d3530b9cd1b8b1a04b953d31b7efac
Thibaut FONTAINE <tfontain@e3r6p17.42.fr>
2017-04-06 16:22:20 +0200

    solver.c piece vide segfault corrige

--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 01:29:47 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/01 06:37:54 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/05 17:42:42 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,10 @@ int			check_column(char **array, size_t column, char c, size_t size)
 
 	i = 0;
 	--size;
-	while (i < size && array[i][column] != c)
+	while (array[i][column] && i < size && array[i][column] != c)
 		++i;
+	if (array[i][column] == 0)
+		return (-1);
 	return (i < size);
 }
 
@@ -59,8 +61,10 @@ int			check_line(char **array, size_t line, char c, size_t size)
 	size_t	i;
 
 	i = 0;
-	while (i < size && array[line][i] != c)
+	while (array[line][i] && i < size && array[line][i] != c)
 		++i;
+	if (array[line][i] == 0)
+		return (-1);
 	return (i < size);
 }
 
@@ -140,12 +144,20 @@ int			delete_down(char **array, int *size)
 
 int			leftup_piece(t_array *t)
 {
-	while (check_line(t->piece, 0, t->j, t->szpiece.x) == 0)
+	int		r;
+
+	while ((r = check_line(t->piece, 0, t->j, t->szpiece.x)) == 0)
 		delete_up(t->piece, &(t->szpiece.y));
-	while (check_column(t->piece, 0, t->j, t->szpiece.y) == 0)
+	if (r == -1) // piece vide
+		return (-1);
+	while ((r = check_column(t->piece, 0, t->j, t->szpiece.y)) == 0)
 		delete_left(t->piece, &(t->szpiece.x));
-	while (check_line(t->piece, t->szpiece.y - 1, t->j, t->szpiece.x) == 0)
+	if (r == -1)
+		return (-1);
+	while ((r = check_line(t->piece, t->szpiece.y - 1, t->j, t->szpiece.x)) == 0)
 		delete_down(t->piece, &(t->szpiece.y));
+	if (r == -1)
+		return (-1);
 	//while (check_column(t->piece, t->szpiece.x - 1, t->j, t->szpiece.y) == 0) // probleme pour la
 	//	delete_right(t->piece, &(t->szpiece.x));                                // suppression a droite
 	return (1);
