e2528dc8295ef6c2c52fd10298288f64c27bff34
Thibaut FONTAINE <tfontain@e2r10p2.42.fr>
2017-04-10 08:42:24 +0200

    ok

---
+++ mathfuncs.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mathfuncs.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/10 07:54:54 by tfontain          #+#    #+#             */
+/*   Updated: 2017/04/10 08:42:06 by tfontain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./filler.h"
+
+/*
+** calculate a good approximation of sqrt
+** note : this is black magic
+*/
+
+float			q_sqrt(const float x)
+{
+	const float	xhalf = 0.5f * x;
+	t_sqrt		u;
+
+	u.x = x;
+	u.i = 0x5f3759df - (u.i >> 1);
+	return (x * u.x * (1.5f - xhalf * u.x * u.x));
+}
+
+/*
+** calculate the distance between two points
+*/
+
+float			distance(t_size a, t_size b)
+{
+	const double d1 = a.x - b.x;
+	const double d2 = a.y - b.y;
+
+	return (q_sqrt(d1 * d1 + d2 * d2));
+}
