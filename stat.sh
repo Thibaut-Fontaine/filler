8686e9ec7955ff8c36262413734943cd84c4b760
Thibaut FONTAINE <tfontain@e2r11p7.42.fr>
2017-04-12 00:21:12 +0200

    stat shell

---
+++ stat.sh
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    stat.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tfontain <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2017/04/11 23:38:53 by tfontain          #+#    #+#              #
+#    Updated: 2017/04/12 00:20:52 by tfontain         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+player1=carli.filler
+player2=tfontain.filler
+
+n_turns=20
+map=01
+
+# dont touch after this line --------------------------------------------------
+
+p1=0;
+p2=0;
+
+while [ $n_turns != 0 ]
+do
+	./resources/filler_vm -f ./resources/maps/map$map -p1 \
+		./resources/players/$player1 -p2 \
+		./resources/players/$player2
+	result=$(cat filler.trace | grep won)
+	if echo "$result" | grep -q "$player1"
+	then
+		((p1++))
+	else
+		((p2++))
+	fi
+	((n_turns--))
+done
+
+echo $result
+echo $player1
+echo $p1
+echo $player2
+echo $p2
