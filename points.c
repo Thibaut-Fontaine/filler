2a4617af8c84cc31c1d06f16cac909ca820ca8c9
Thibaut FONTAINE <tfontain@e2r11p7.42.fr>
2017-04-11 20:37:47 +0200

    fix segfault if list was NULL

--- points.c
+++ points.c
@@ -6,7 +6,7 @@
 /*   By: tfontain <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/10 11:29:19 by tfontain          #+#    #+#             */
-/*   Updated: 2017/04/11 18:59:53 by tfontain         ###   ########.fr       */
+/*   Updated: 2017/04/11 20:27:51 by tfontain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ t_list		*fill_points(char **array, char c)
 /*
 ** compare all the enemy points and the coordonates where we can place
 ** to get the two nearest points, then return the player's one.
+** return -666 && -666 if one of the list == NULL (means error)
 */
 
 t_size		reach_nearest(t_array t, t_list *adv)
@@ -58,12 +59,17 @@ t_size		reach_nearest(t_array t, t_list *adv)
 	float	dist;
 	float	tmp;
 	t_list	*hadv;
+	t_list	*hpla;
 	t_size	ret;
 
 	pla = fill_points(t.plateau, t.j);
-	// il faut free les deux listes chainees
-	hadv = adv;
-	dist = 999999999;
+	hadv = adv; // to free
+	hpla = pla; // to free
+	ret.x = -666;
+	ret.y = -666;
+	if (pla == NULL || adv == NULL)
+		return (ret);
+	dist = 99999999;
 	while (pla != NULL)
 	{
 		adv = hadv;
@@ -79,5 +85,6 @@ t_size		reach_nearest(t_array t, t_list *adv)
 		}
 		pla = pla->next;
 	}
+	// il faut free hadv et hpla !
 	return (ret);
 }
